582881ec750f63e9170cb1efab2b70d36f7803ff
achengli <tao3000ac@protonmail.com>
2025-04-20 23:21:54 +0200

    incremental

--- maxpack.sh
+++ maxpack.sh
@@ -15,9 +15,19 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-export DEFAULT_CONFIG=$HOME/.local/share/maxpack/default/maxpack.cfg
-export MAXPACK_RAW_GITHUB_URL='https://raw.githubusercontent.com/achengli/maxpack/refs/heads/main'
+export MAXPACK_HOME=$HOME/.config/maxpack
+export DEFAULT_CONFIG=$MAXPACK_HOME/maxpack.cfg
 export MAXPACK_HOST_URL="NOT IMPLEMENTED"
+
+function echoerr(){
+  local echomsg=$1
+  if [ -z "$echomsg" ]; then
+    echoerr "echoerr: bad usage"
+  else
+    echo -e '\033[0;31m[ERROR] - \033[0m'$echomsg
+  fi
+  exit $2
+}
 
 function maxpack-load-config(){
 """
@@ -40,6 +50,7 @@ Load maxpack configuration with priority order:
       maxpack--read-config $DEFAULT_CONFIG
     fi
   fi
+  export maxpack_config_loaded=yes
 }
 
 function maxpack--read-config(){
@@ -49,63 +60,33 @@ Read config file exporting it to bash env variables.
   local config_file=$1
   while read line
   do
-    local var=$(echo $line | cut -d'=' -f1)
-    var=${var^^}
-    local content=$(echo $line | cut -d'=' -f2)
-    eval "export ${var}='${content}'"
+    local var=$(echo $line | cut -d'=' -f1 | xargs)
+    var=${var^^} # to uppercase
+    local content=$(echo $line | cut -d'=' -f2 | xargs)
+    eval "export ${var}=\"${content}\""
   done < "$config_file"
 }
 
 function maxpack-install(){
   local package_name=$1
   local package_version=$2
-  maxpack-load-config
+
+  if [ -z "$maxpack_config_loaded" ]; then
+    maxpack-load-config
+  fi
 
   if [ -z "$package_name" ]; then
-    echo "You must pass the name or url of the package"
-    return -1
+    echoerr "maxpack-install: You must pass a package name and OPTIONALLY a version of the package" -1
   elif [ -z "$package_version" ]; then
-    echo "Latest version will be installed"
+    echo "Latest version of ${package_name:0:20} will be installed"
+    package_version=latest
   fi
 
-  # `out_code' mark which was the point the package wasn't found.
-  local out_code=0
-  # search locally
-  if [ -e $MAXPACK_HOME/.cache/packages.list ]; then
-    local package
-    while read line
-    do
-      if [ -n "$(echo $line | grep -E $package_name)" ]; then
-        $remote_package=$(echo $line | grep -Eo "[[:alnum:]_/\.-]+\$")
-      else
-        out_code=1
-      fi
-    done < $MAXPACK_HOME/.cache/packages.list
-  else
-    curl -o $MAXPACK_RAW_GITHUB_URL'/cfg/packages.list'
-  fi
-  # search in remote package collection
-  
-  while read line
-  do
-    echo $line
-  done < $(curl $MAXPACK_HOST)
-  local remote_package=$(curl $MAXPACK_HOST_URL) | grep -Eo "[[:alnum:]_/\.-]+\$")
-  if [ -z "$remote_package" ];then 
-    out_code=2
+  if ![ -f $MAXPACK_HOME/remote.list ]; then
+    echoerr "maxpack-install: No remote is defined" -1
   fi
 
-  # go to the url directly
-  if [ -z "$(curl $package)" ]; then  
-    out_code=3
-  fi
-
-  if [ $out_code == 0 ]; then
-  elif [ $out_code == 1 ]; then
-  elif [ $out_code == 2 ]; then
-  else
-  fi
-
+  for f in $MAXPACK
 }
 
 function maxpack-remove(){
